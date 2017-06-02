18cd55fecf9e48c8b849072a684de589db503310
Shaurya <shauryachats@gmail.com>
2017-06-02 13:12:38 +0530

    removed scrollbars

--- scr.js
+++ scr.js
@@ -1,34 +1,42 @@
 var paneindex = 0
 
-$(document).ready(function() {
-
-	$(document).keydown(function(e) {
-		if (e.keyCode == 38)
-		{
-			if (paneindex > 0)
-			{
-				--paneindex;
-				animatepanes($('.panes')[paneindex]);
-			}
-			e.preventDefault();
-		} 
-		else if (e.keyCode == 40)
-		{
-			if (paneindex < $('.panes').length-1)
-			{
-				++paneindex;
-				animatepanes($('.panes')[paneindex]);
-			}
-			e.preventDefault();
-		}
-		console.log(paneindex);
-	});
-
-});
-
-animatepanes = function(obj) {
-	console.log(obj);
+animatepanes = function() {
 	$('html, body').animate({
-		scrollTop: obj.offsetTop
+		scrollTop: $('.panes')[paneindex].offsetTop
 	}, 1000, 'swing');
 }
+
+panedown = function() {
+	if (paneindex < $('.panes').length-1)
+	{
+		++paneindex;
+		animatepanes();
+	}
+}
+
+paneup = function() {
+	if (paneindex > 0)
+	{
+		--paneindex;
+		animatepanes();
+	}
+}
+
+$(document).keydown(function(e) {
+	if (e.keyCode == 38) //up arrow key
+	{
+		paneup();
+		e.preventDefault();
+	} 
+	else if (e.keyCode == 40) //down arrow key
+	{
+		panedown();
+		e.preventDefault();
+	}
+	console.log(paneindex);
+});
+
+//for mobile browsers
+$(document).on('swipe', function(event) {
+	console.log(event);
+});
